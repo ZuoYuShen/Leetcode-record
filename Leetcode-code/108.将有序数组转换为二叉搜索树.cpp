3eafef8852ab18f02a2c4859f344734c34fdb217
ZuoYuShen <625080891@qq.com>
2020-07-03 16:00:20 +0800

    Code for #108

---
+++ Leetcode-code/108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums){
+        return helper(nums, 0, nums.size()-1);
+    }
+
+    TreeNode* helper(vector<int>& nums, int left, int right){
+        if(left > right) return nullptr;
+        int mid = (left + right) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = helper(nums, left, mid-1);
+        root->right = helper(nums, mid+1, right);
+        return root;
+    } 
+};
+// @lc code=end
